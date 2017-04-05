cb8339a7db36817d266d463282a872dc551032bd
sunzhonghui <sunzhonghui@idmiss.com>
2017-04-05 14:13:20 +0800

    init submit

---
+++ src/test/java/com/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringbootApplication.class)
+public class SpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
